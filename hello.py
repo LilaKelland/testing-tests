c9ca795b67ba93fc79e56b10bc7bb48155eb83f1
Lila Kelland <Lila.Kelland@gmail.com>
2021-03-29 11:19:53 -0300

    now works

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 from random import choices
 
+
 def with_input(color):
 
     words = ["red", "yellow", "green"]
     new_color = choices(words)[0]
     color_dict = {"old": color, "new": new_color}
     return color_dict
+
 
 if __name__ == "__main__":
     print(with_input("blue"))
--- test_hello.py
+++ test_hello.py
@@ -2,15 +2,17 @@ from hello import with_input
 from cli import callcolor
 from click.testing import CliRunner
 
+
 def test_with_input():
     """This is my library"""
-    
+
     result = with_input("blue")
     assert result["old"] == "blue"
+
 
 def test_cli():
     """This is my command-line tool"""
     runner = CliRunner()
-    result = runner.invoke(callcolor, ['--color', 'blue'])
+    result = runner.invoke(callcolor, ["--color", "blue"])
     assert result.exit_code == 0
-    assert 'blue' in result.output
+    assert "blue" in result.output
